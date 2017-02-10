c9e3cc00666b6619f19016d691aead7b47c8259d
KwonTaeSeong <xotjd183@gmail.com>
2017-02-10 13:52:51 +0900

    implements login and account - 2

---
+++ js/background.js
@@ -0,0 +1,159 @@
+// Define Method and Call for Information Hidding
+var k;
+(function(){
+	// Define TYPE
+	REQUEST = "request";
+	RESPONSE = "response";
+	CONNECT = "connect";
+	CLOSE_POPUP = "close_popup";
+	
+	// Define Category
+	STATUS = "current_status";
+	CAT_LOGIN = "popup_login";
+	CAT_JOIN = "popup_join";
+	//ETC...
+	USER_ACCOUNT = "account_info";
+	LOGIN = "Login";
+	NOT_LOGIN = "NotLogin";
+
+	//
+	REQUEST_LOGIN = "request:login";
+	RESPONSE_LOGIN = "response:login";
+	REQUEST_JOIN = "request:join";
+	RESPONSE_JOIN = "response:join";
+
+	// Start Define User Informateion
+	var User = function(){
+		this.__constructor();
+	}
+	User.prototype.myStatus;
+	User.prototype.accountInfo;
+	User.prototype.isOpen;
+	User.prototype.__constructor = function(){
+		User.prototype.myStatus = NOT_LOGIN;
+		User.prototype.accountInfo = null;
+		User.prototype.isOpen = false;
+	}
+	// End Define User Information
+
+	console.log("Start!");
+	// Connect to Server on Websocket
+	var socket = io.connect("http://localhost:9900");
+	var user = new User();
+	var myPort;
+	// Connect to popup Page
+	
+	socket.on(CONNECT, function(data){
+		console.log("Connect to Websocket");
+		// chrome.storage.sync.set({ "account_info" : {id : "kts", pwd : "1234"}}, function(data){});
+		chrome.storage.sync.get( USER_ACCOUNT, (data) => {
+			console.log("Check "+USER_ACCOUNT+" : ", data);
+			// Check User Account
+			if(data[USER_ACCOUNT]){ 
+				user.accountInfo = data[USER_ACCOUNT];	
+				socket.emit(REQUEST_LOGIN, user.accountInfo);
+			}
+
+			//RESPONESE LOGIN
+			socket.on(RESPONSE_LOGIN, (data) => {
+				console.log("=> Response|login : ", data);
+				if(data.isSuccess == true){
+					user.myStatus = LOGIN;
+				}else chrome.storage.sync.clear();
+				
+			});
+			// define server and background communication
+			socket.on(RESPONSE_JOIN, (res) => {
+				var data = {
+					type 		:  RESPONSE,
+					category 	: CAT_JOIN,
+					isSuccess 	: res.isSuccess,
+					msg			: res.msg
+				};
+				myPort.postMessage(data);
+			});
+			socket.on(RESPONSE_LOGIN, (res) => {
+				var data = {
+					type 		:  RESPONSE,
+					category 	: CAT_LOGIN,
+					isSuccess 	: res.isSuccess,
+				};
+				if(res.isSuccess){
+					data['userProfile'] = res['userProfile'];
+					data['friendList'] = res['friendList'];
+				}
+				myPort.postMessage(data);
+			});
+			// Define PopupPage OnConnect Event
+			chrome.runtime.onConnect.addListener(function(port){
+				if(port.name == "magpie_app"){
+					myPort = port;
+				 	init(socket, port);
+				}
+
+			});
+			// chrome.runtime
+		});	
+	});
+
+	// Initialize Communication Between Background and Popup Page
+	function init(client, port){ 
+		/*
+			port : communication to background and popup
+			client : communication to Server
+		*/
+		console.log("connect!", user.myStatus);
+		user.isOpen = true;
+
+		// define background and popup communication 
+		myPort.postMessage({type : CONNECT, isSuccess : true});
+		myPort.onMessage.addListener(function(data){
+			if(data.type == REQUEST){
+				requestFunction(data,client);
+			}else if(data.type == CLOSE_POPUP){
+				// console.log("close window");
+				user.isOpen = false;
+			}
+		});
+		myPort.onDisconnect.addListener(() => {
+			console.log("close");
+			user.isOpen = false;
+		//	eventDelete()
+		});
+	}
+
+	function requestFunction(data, client){
+		if(data.category == STATUS){
+			var res = {
+				type : RESPONSE,
+				category : STATUS,
+				status : user.myStatus
+			}
+			myPort.postMessage(res);
+		}else if(data.category == CAT_LOGIN)
+			wRequestLogin(data['loginEmail'], data['loginPwd'], client);
+		else if(data.category == CAT_JOIN)
+			wRequestJoin(data['input_name'], data['input_email'], 
+				data['input_pwd1'], data['input_pwd2'], client);
+		
+	}
+
+	function wRequestLogin(userID, userPwd, wSocket){
+		var userAccount = {
+			loginEmail : userID,
+			loginPwd : userPwd
+		};
+		console.log("data : ",userAccount);
+		wSocket.emit(REQUEST_LOGIN, userAccount);
+	}
+	function wRequestJoin(inputName, inputEmail, inputPwd1, inputPwd2, wSocket){
+		var inputData = {
+			'inputName' 	: inputName,
+			'inputEmail' 	: inputEmail,
+			'inputPwd1' 	: inputPwd1,
+			'inputPwd2' 	: inputPwd2
+		};
+		console.log("data : ", inputData);
+		wSocket.emit(REQUEST_JOIN, inputData);
+	}
+})();
